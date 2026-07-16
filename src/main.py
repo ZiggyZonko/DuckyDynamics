6561f94f48c94bbfabd3af229d69ee667b9634ee
ZiggyZonko <zacharyalindley@gmail.com>
2026-07-16 21:30:39 +1000

    First commit

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 import pygame
 import sys
 from CONSTANTS import *
+from physics import *
+
+# ---- Object Arrays ---- #
+balls = []
+squares = []
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("🦆 DuckyDynamics")
+
+ball = Ball(SCREEN_WIDTH, SCREEN_HEIGHT)
+square = Square(SCREEN_WIDTH, SCREEN_HEIGHT)
+squares.append(square)
+balls.append(ball)
 
 clock = pygame.time.Clock()
 
@@ -15,8 +25,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_pos = event.pos
+
     screen.fill((40, 44, 52))
+
+    pygame.draw.circle(screen, ball.color, (int(ball.x), int(ball.y)), ball.radius)
+    pygame.draw.rect(screen, square.color, (int(square.x), int(square.y), square.radius, square.radius))
+
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
 sys.exit()
--- src/physics.py
+++ src/physics.py
@@ -1,1 +1,22 @@
 # Use for Physics Functions
+import random
+from CONSTANTS import *
+
+class Ball:
+    def __init__(self, x, y):
+        self.x = random.randint(0, x)
+        self.y = random.randint(0, y)
+
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.radius = random.randint(10,20)
+
+
+class Square:
+    def __init__(self, x, y):
+        self.x = random.randint(0, x)
+        self.y = random.randint(0, y)
+
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.radius = random.randint(10,20)
